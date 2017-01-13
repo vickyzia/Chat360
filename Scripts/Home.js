78fbf94341e9b6dba91455c9aa4bf7f52219441b
vickyzia <m.vickyzia@gmail.com>
2017-01-13 10:22:05 +0500

    UI update

---
+++ Scripts/Home.js
@@ -0,0 +1,25 @@
+﻿var selectedUser = "";
+$(document).ready()
+{
+    $('#video-container').hide();
+    $('#centerDiv').hide();
+   
+    $('.list-group a').click(function (e) {
+        e.preventDefault()
+
+        $that = $(this);
+
+        $that.parent().find('a').removeClass('active');
+        $that.addClass('active');
+        var val = $that.attr("data-username");
+        var img = $that.parent().find('img').attr("src");
+        selectedUser = val;
+        console.log(val);
+        $('#centerDiv').show();
+        $('#img-circle').attr('src', img);
+        $('#infoMessage').text('Click To Call');
+        $('#username').text(val);
+    });
+
+
+}
